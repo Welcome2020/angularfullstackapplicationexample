ce8016d7fd087bac197b41a5ba01443ba0bec217
Karthik Shankar Prasad <karthik.shankar00@gmail.com>
2020-09-17 10:52:31 -0400

    first commit

---
+++ src/app/create-employee/create-employee.component.ts
@@ -0,0 +1,40 @@
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-create-employee',
+  templateUrl: './create-employee.component.html',
+  styleUrls: ['./create-employee.component.scss']
+})
+export class CreateEmployeeComponent implements OnInit {
+
+  employee: Employee = new Employee();
+  submitted = false;
+
+  constructor(private employeeService: EmployeeService,
+    private router: Router) { }
+
+  ngOnInit() {
+  }
+
+  saveEmployee(){
+    this.employeeService.createEmployee(this.employee).subscribe(data =>{
+      console.log(data);
+    },
+
+    error => console.log(error));
+  }
+
+  getToEmployeeList(){
+    this.router.navigate(['/employees']);
+  }
+
+  
+  onSubmit(){
+    //console.log(this.employee);
+    this.saveEmployee();
+    this.getToEmployeeList();
+  }
+}
